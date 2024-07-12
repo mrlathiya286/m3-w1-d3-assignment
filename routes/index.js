6884e800b088095780b8090e99cc4f9db11aa618
mrlathiya286 <mauliklathiya25@gmail.com>
2024-07-12 11:38:16 -0700

    updated the code

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const router = express.Router();
 const { check, validationResult } = require("express-validator");
 const { route } = require("../app");
